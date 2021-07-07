6d428b1262afd4ad46594df4d3839a1d7835d80a
André Moura <andre.benedicto@etec.sp.gov.br>
2021-07-07 11:56:30 -0300

    converter id em inteiro na Categoria

--- Models/Category.php
+++ Models/Category.php
@@ -6,6 +6,20 @@
 
     class Category extends Model
     {
+        public function __get($name)
+        {
+            return $this->$name ?? null;
+        }
+
+        public function __set($name, $value)
+        {
+            if ($name == 'id') {
+                $this->$name = intval($value);
+            } else {
+                $this->$name = $value;
+            }
+        }
+
         public function getAll()
         {
             $sql = "SELECT * FROM categories";
